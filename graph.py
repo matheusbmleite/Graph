b5d70c2537d3cb5c2d68b2e12aac964a16445096
Matheus Ben-Hur <matheusb@gary.home>
2015-04-07 21:50:22 -0300

    Derivated actions added

--- graph.py
+++ graph.py
@@ -37,3 +37,41 @@ class Graph:
 	def degree(self, vertex):
 		return len(self.adjacents(vertex))
 
+	def is_regular(self):
+		degree = self.order(self.a_vertex())
+		for vertex in self.vertex:
+			if self.order(vertex) != degree:
+				return False
+		else:
+			return True
+
+	def is_full(self):
+		vertexes = self.vertexes
+		order = self.order
+		for vertex in vertexes:
+			if self.degree(vertex) == (order - 1):
+				return False
+		else:
+			return True
+	def transitive_closure(self, vertex): 
+		def _transitive_closure_(vertex, visited):
+			visited.add(vertex)
+			for vertex_aux in self.adjacents(vertex):
+				if not vertex_aux in visited:
+					_transitive_closure_(vertex_aux, visited)
+			return visited
+		return _transitive_closure_(vertex, set())
+
+	def is_connected(self):	
+		return len(self.vertexes) == len(self.transitive_closure(self.a_vertex()))
+
+	def is_tree(self):
+		def _is_tree_(actual_vertex, visited):
+			for vertex in self.adjacents(actual_vertex):
+				if vertex in visited:
+					return False
+				visited.add(vertex)
+				_is_tree_(vertex, visited)
+			return True
+
+		
